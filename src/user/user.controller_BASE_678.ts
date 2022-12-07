e7588ad61d600d8c516748ae92a7a00c59b67049
nedu <nedufari@gmail.com>
2022-12-07 08:20:18 +0100

    updated user entities, dto, user service and controllers

---
+++ src/user/user.controller_BASE_678.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateNewUserDto, LoginDto } from '../auth/authdto';
+import { GetUser } from '../auth/passport/jwtDecorator';
+import { JwtGuard } from '../auth/passport/jwtGuard';
+import { User } from '../entities/user.entity';
+import { UserService } from './user.service';
+import { CreateUserDto, HospitalCreateDto, LaboratoryCreateDto, PharmacyCreateDto } from './userdto';
+
+
+@Controller('user')
+export class UserController {
+    constructor(private userservice:UserService){}
+
+
+    @UseGuards(JwtGuard)
+    @Get('me')
+    me (@GetUser()user:User,){
+        return user
+    }
+
+    @Post('signup')
+    async Signup(@Body()signupdto:CreateUserDto,hopsitaldto:HospitalCreateDto, labdto:LaboratoryCreateDto,pharmadto:PharmacyCreateDto){
+        return await this.userservice.Signup(signupdto,hopsitaldto,labdto,pharmadto)
+    }
+   
+}
